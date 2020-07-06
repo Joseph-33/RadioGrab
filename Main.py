bd3ba732121c6b74cc90eed26e9f9ee401501819
unknown <josephjosephandrews@gmail.com>
2020-07-06 14:23:06 +0100

    Updated SpotipyModules and Main

--- Main.py
+++ Main.py
@@ -2,10 +2,12 @@ import spotipy
 import yaml
 import spotipy.util as util
 from MusicGrab import music_grab
+from SpotipyModules import get_top_songs_for_artist, playlist_id, convert_to_uri, split_artist
 
 stream = open("config.yaml") # Opens the config Path
 user_config = yaml.safe_load(stream) # Safe Loads using yaml
 user_id = user_config['username'] # username
+
 token = util.prompt_for_user_token(user_config['username'], scope='playlist-modify-private,playlist-read-private', client_id=user_config['client_id'], client_secret=user_config['client_secret'], redirect_uri=user_config['redirect_uri']) # Generates a token
 sp = spotipy.Spotify(auth=token) # Create a Spotipy Instance
 
@@ -14,14 +16,15 @@ sp = spotipy.Spotify(auth=token) # Create a Spotipy Instance
 
 personal_playlist_name = user_config["personal_playlist_name"] # The name of the users personal spotify playlist to overwrite and save the songs to
 my_playlist_id = playlist_id(personal_playlist_name)  # Gets the playlist id from the name
+
 artist_Songs = music_grab(url = user_config["radio_url"]) # Obtains the music songs from onlineradiobox
-artist_results = get_top_songs_for_artist(artist_Songs) # Obtain spotipy searches from the artist_song list
+artist_results = get_top_songs_for_artist(artist_Songs,sp) # Obtain spotipy searches from the artist_song list
 tracklist = convert_to_uri(artist_results) # Obtain song uri's from the spotipy searches
 
 
 
 
-sp.user_playlist_replace_tracks(user_id,my_playlist_id,[]) # Clear the playlist
+sp.user_playlist_replace_tracks(user_id,my_playlist_id,[]) # Clears the playlist
 
 chunk_size = 99 # Spotify can only accept 100 tracks
 for i in range(0, len(tracklist), chunk_size):
